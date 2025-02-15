e04dc00825762e6ce239b37aa0523ea06e9a61fb
devgembaware <dev@gembaware.com>
2025-02-15 09:56:17 +0100

    add profile section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import {ButtonModule} from 'primeng/button';
 import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from './header/header.component';
-
+import {ProfileComponent} from './profile/profile.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonModule, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, ProfileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
