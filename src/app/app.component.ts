2b54421dc941bfad34c1ece79f8f050e32a95e28
devgembaware <dev@gembaware.com>
2025-02-16 22:43:39 +0100

    implement sections

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,11 @@ import {HeaderComponent} from './header/header.component';
 import {ProfileComponent} from './profile/profile.component';
 import { EducationComponent } from "./education/education.component";
 import { ExperienceComponent } from "./experience/experience.component";
+import { SkillComponent } from "./skills/skill.component";
+import { ProjectsComponent } from "./projects/projects.component";
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, ProfileComponent, EducationComponent, ExperienceComponent],
+  imports: [RouterOutlet, HeaderComponent, ProfileComponent, EducationComponent, ExperienceComponent, SkillComponent, ProjectsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
